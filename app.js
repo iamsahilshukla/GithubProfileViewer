9d21927f0c9293cf134d97cdb51a8c004cf28a74
iamsahilshukla <iamsahilshukla@gmail.com>
2023-04-26 03:25:58 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,58 @@
+angular.module('githubProfileViewer', ['ngRoute', 'ngMaterial'])
+    .config(function ($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'search-profiles.html',
+                controller: 'SearchProfilesCtrl as search'
+            })
+            .when('/profile/:username', {
+                templateUrl: 'view-profile.html',
+                controller: 'ViewProfileCtrl as view'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    })
+    .controller('SearchProfilesCtrl', function ($http, $location) {
+        var vm = this;
+
+        vm.searchUsername = '';
+        vm.searchResults = [];
+
+        vm.searchProfiles = function () {
+            $http.get('https://api.github.com/search/users?q=' + vm.searchUsername)
+                .then(function (response) {
+                    vm.searchResults = response.data.items;
+                });
+        };
+
+        vm.viewProfile = function (username) {
+            var encodedUsername = encodeURIComponent(username);
+            $location.path('/profile/' + encodedUsername);
+        };
+
+    })
+    .controller('ViewProfileCtrl', function ($http, $routeParams) {
+        var vm = this;
+
+        vm.username = $routeParams.username;
+        vm.profile = {};
+        vm.repositories = [];
+        console.log(vm.username);
+        $http.get('https://api.github.com/users/' + 'iamsahilshukla')
+            .then(function (response) {
+                vm.profile = response.data;
+            })
+            .catch(function (error) {
+                console.error(error);
+            });
+
+
+        $http.get('https://api.github.com/users/' + 'iamsahilshukla' + '/repos')
+            .then(function (response) {
+                vm.repositories = response.data;
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    });
